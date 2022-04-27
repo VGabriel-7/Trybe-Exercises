18e27087da2f11c0ccf3db927ad45822ad51b622
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-04-27 16:09:08 -0300

    Implementando validação no Form com bootstrap

--- Fundamentos/bloco-6-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1-html-css-forms/exercicio-1-trybe-trip/script.js
+++ Fundamentos/bloco-6-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-1-html-css-forms/exercicio-1-trybe-trip/script.js
@@ -1,5 +1,21 @@
-const button = document.querySelector('#button-submit');
 
-button.addEventListener('click', function (event) {
-  event.preventDefault();
-});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
